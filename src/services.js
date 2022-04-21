4b45fe381ec185b4479b893ac089d5c5e1563910
Thomas Regourd <tom.reg@hotmail.fr>
2022-04-21 10:23:29 +0200

    first display of timesheet list with axios /GET  request

--- src/services.js
+++ src/services.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:1337"; // api.myamazingwebsite.com
+const baseURL = "http://localhost:1337";
 
 const base = axios.create({ baseURL });
 
 const services = {
   getUsersList() {
     return base.get(`/users`).then((res) => res.data);
+  },
+  getAllTimesheetList() {
+    return base.get(`/timesheet/all`).then((res) => res.data);
   },
 };
 
