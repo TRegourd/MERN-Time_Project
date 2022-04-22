0494b40c660db3a47f6f4f7862f179ac115f94f8
Thomas Regourd <tom.reg@hotmail.fr>
2022-04-22 13:49:18 +0200

    login data  get information from body

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,39 @@
-import * as React from "react";
+import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
 import services from "../services";
+import { useState } from "react";
 
 export default function Login() {
-  let body = { email: "marrington3@youtube.com", password: "dQHYdc3rBZX" };
+  //let body = { email: "marrington3@youtube.com", password: "dQHYdc3rBZX" };
+
+  const [body, setBody] = useState({
+    email: "",
+    password: "",
+  });
 
   function handleSubmit(e) {
     e.preventDefault();
     console.log(body);
     services
       .login(body)
-      .then(() => {})
+      .then(() => {
+        alert("logged");
+      })
       .catch((err) => {
         console.log(err);
-        alert("oups");
+        alert("incorrect login");
       });
+  }
+
+  function updateBody(key, value) {
+    setBody({ ...body, [key]: value });
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    updateBody(name, value);
   }
 
   return (
@@ -28,20 +45,24 @@ export default function Login() {
       noValidate
       autoComplete="off"
       onSubmit={handleSubmit}
+      onChange={handleChange}
     >
       <h1>Login</h1>
+      <pre>{JSON.stringify(body, null, 2)}</pre>
       <div>
         <TextField
           required
           id="outlined-email-required"
           label="Email"
           type="email"
+          name="email"
         />
         <TextField
           id="outlined-password-input"
           label="Password"
           type="password"
           autoComplete="current-password"
+          name="password"
         />
       </div>
       <Button type="submit" variant="outlined">
