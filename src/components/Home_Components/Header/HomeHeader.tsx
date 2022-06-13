02ef8059a0256d2a25bd270d227bf1eb5eab0e0a
Thomas Regourd <tom.reg@hotmail.fr>
2022-06-13 14:09:47 +0200

    modified get started link when logged

--- src/components/Home_Components/Header/HomeHeader.tsx
+++ src/components/Home_Components/Header/HomeHeader.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./HomeHeader.css";
 import Link from "react-scroll/modules/components/Link";
 import { Link as RouterLink } from "react-router-dom";
 import { Button } from "@mui/material";
 import BgImage from "../../../assets/backgroundEvents.jpeg";
+import { AuthContext, AuthContextType } from "../../../AuthProvider";
 
 export default function HomeHeader() {
+  const { logged } = useContext(AuthContext) as AuthContextType;
   return (
     <div className="header-container">
       <img className="bg-image" alt="headerBackground" src={BgImage}></img>
@@ -14,11 +16,20 @@ export default function HomeHeader() {
       <p>Keep track of your time</p>
       <div className="header-btns">
         <Button className="btns"></Button>
-        <RouterLink to="/login">
-          <Button className="btns contained" variant="contained">
-            Get Started
-          </Button>
-        </RouterLink>
+        {!logged && (
+          <RouterLink to="/login">
+            <Button className="btns contained" variant="contained">
+              Get Started
+            </Button>
+          </RouterLink>
+        )}
+        {logged && (
+          <RouterLink to="/dashboard">
+            <Button className="btns contained" variant="contained">
+              Get Started
+            </Button>
+          </RouterLink>
+        )}
         <Link to="contact" smooth>
           <Button className="btns outlined" variant="outlined">
             CONTACT US
